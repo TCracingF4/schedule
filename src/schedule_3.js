5a5668c49292945e4fe1e624988c4b9c5d1d1aec
TCracingF4 <data@tcracingf4.onmicrosoft.com>
2025-04-15 19:37:47 +0200

    OCT_BCN_D1

--- src/schedule_3.js
+++ src/schedule_3.js
@@ -1,170 +1,24 @@
 const schedule = [
     {
-        "Start": "08:10:00",
-        "Ends": "08:10:00",
-        "Event": "Start Engines",
-        "Team": "Mechanics",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "08:15:00",
-        "Ends": "08:15:00",
-        "Event": "Drivers in the car",
-        "Team": "Drivers",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "08:30:00",
-        "Ends": "08:50:00",
-        "Event": "Official Previous Test 3",
-        "Gap": "03:45:00",
-        "Team": "Session",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:02:00",
-        "Ends": "12:02:00",
-        "Event": "Start Engines",
-        "Team": "Mechanics",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:07:00",
-        "Ends": "12:07:00",
-        "Event": "Drivers in the car",
-        "Team": "Drivers",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:22:00",
-        "Ends": "12:22:00",
-        "Event": "Pit Lane Opens",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:27:00",
-        "Ends": "12:27:00",
-        "Event": "Pit Lane Closes-Grid",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:27:00",
-        "Ends": "12:27:00",
-        "Event": "5 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:29:00",
-        "Ends": "12:29:00",
-        "Event": "3 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:31:00",
-        "Ends": "12:31:00",
-        "Event": "1 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "12:35:00",
-        "Ends": "12:55:00",
-        "Event": "Qualifying Session 1",
-        "Gap": "00:20:00",
-        "Team": "Session",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "13:30:00",
-        "Ends": "14:30:00",
-        "Event": "Lunch Break",
+        "Start": "09:00:00",
+        "Ends": "09:55:00",
+        "Event": "Official Previous Test 1",
         "Team": "All",
         "Digital": "Yes",
         "Day": 3.0
     },
     {
-        "Start": "16:42:00",
-        "Ends": "16:42:00",
-        "Event": "Start Engines",
-        "Team": "Mechanics",
+        "Start": "11:00:00",
+        "Ends": "12:25:00",
+        "Event": "Official Previous Test 2",
+        "Team": "All",
         "Digital": "Yes",
         "Day": 3.0
     },
     {
-        "Start": "16:47:00",
-        "Ends": "16:47:00",
-        "Event": "Drivers in the car",
-        "Team": "Drivers",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "17:02:00",
-        "Ends": "17:02:00",
-        "Event": "Pit Lane Opens",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "17:07:00",
-        "Ends": "17:07:00",
-        "Event": "Pit Lane Closes-Grid",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "17:07:00",
-        "Ends": "17:07:00",
-        "Event": "5 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "17:09:00",
-        "Ends": "17:09:00",
-        "Event": "3 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "17:11:00",
-        "Ends": "17:11:00",
-        "Event": "1 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "17:15:00",
-        "Ends": "17:50:00",
-        "Event": "Race 1  (30 min. + 1L )",
-        "Team": "Session",
-        "Digital": "Yes",
-        "Day": 3.0
-    },
-    {
-        "Start": "20:00:00",
-        "Ends": "20:00:00",
-        "Event": "Dinner",
+        "Start": "14:55:00",
+        "Ends": "16:20:00",
+        "Event": "Official Previous Test 3",
         "Team": "All",
         "Digital": "Yes",
         "Day": 3.0
--- src/schedule_4.js
+++ src/schedule_4.js
@@ -1,90 +1,25 @@
 const schedule = [
     {
-        "Start": "08:55:00",
-        "Ends": "08:55:00",
-        "Event": "Start Engines",
-        "Team": "Mechanics",
+        "Start": "10:00:00",
+        "Ends": "10:55:00",
+        "Event": "Official Previous Test 4",
+        "Team": "All",
         "Digital": "Yes",
         "Day": 4.0
     },
     {
-        "Start": "08:45:00",
-        "Ends": "08:45:00",
-        "Event": "Drivers in the car",
-        "Team": "Drivers",
+        "Start": "12:30:00",
+        "Ends": "13:55:00",
+        "Event": "Official Previous Test 5",
+        "Team": "All",
         "Digital": "Yes",
         "Day": 4.0
     },
     {
-        "Start": "09:00:00",
-        "Ends": "09:20:00",
-        "Event": "Qualifying Session 2",
-        "Gap": "03:35:00",
-        "Team": "Session",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:22:00",
-        "Ends": "12:22:00",
-        "Event": "Start Engines",
-        "Team": "Mechanics",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:27:00",
-        "Ends": "12:27:00",
-        "Event": "Drivers in the car",
-        "Team": "Drivers",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:42:00",
-        "Ends": "12:42:00",
-        "Event": "Pit Lane Opens",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:47:00",
-        "Ends": "12:47:00",
-        "Event": "Pit Lane Closes-Grid",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:47:00",
-        "Ends": "12:47:00",
-        "Event": "5 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:49:00",
-        "Ends": "12:49:00",
-        "Event": "3 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:51:00",
-        "Ends": "12:51:00",
-        "Event": "1 min. Board",
-        "Team": "Staff",
-        "Digital": "Yes",
-        "Day": 4.0
-    },
-    {
-        "Start": "12:55:00",
-        "Ends": "13:30:00",
-        "Event": "Race 3  (30 min. + 1L )",
-        "Team": "Session",
+        "Start": "16:25:00",
+        "Ends": "17:50:00",
+        "Event": "Official Previous Test 6",
+        "Team": "All",
         "Digital": "Yes",
         "Day": 4.0
     }
